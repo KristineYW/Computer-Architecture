16de75d5865571a2b6e5620e761f3221058f803e
Kristine <ying.kristine.wang@gmail.com>
2020-09-16 09:47:21 -0400

    module 2 assignment

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -14,27 +14,64 @@ class CPU:
         self.ram = [0] * 256
         self.pc = 0
         self.running = True
+        self.opcodes = {
+            0b10000010: self.LDI,
+            0b01000111: self.PRN,
+            0b00000001: self.HLT,
+            0b10100010: self.MUL,
+            0b10100011: self.DIV,
+            0b10100000: self.ADD,
+            0b10100001: self.SUB
+        }
 
     def load(self):
         """Load a program into memory."""
 
-        address = 0
+        # address = 0
 
-        # For now, we've just hardcoded a program:
+        # # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+
+        if len(sys.argv) != 2:
+            print("usage: python3 ls8.py examples/filename")
+            sys.exit(1)
+
+        try:
+            address = 0
+
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    t = line.split('#')
+                    n = t[0].strip()
+
+                    if n == '':
+                        continue
+
+                    try:
+                        n = int(n, 2)
+                    except ValueError:
+                        print(f"Invalid number '{n}'")
+                        sys.exit(1)
+
+                    self.ram[address] = n
+                    address += 1
+
+        except FileNotFoundError:
+            print(f"File not found: {sys.argv[1]}")
+            sys.exit(2)
 
     def ram_read(self, MAR):
         # Should accept the address to read and return the value stored there.
@@ -48,8 +85,13 @@ class CPU:
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+            self.registers[reg_a] += self.registers[reg_b]
+        elif op == "MUL":
+            self.registers[reg_a] *= self.registers[reg_b]
+        elif op == "SUB":
+            self.registers[reg_a] -= self.registers[reg_b] 
+        elif op == "DIV":
+            self.registers[reg_a] //= self.registers[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -72,6 +114,38 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
+
+    def LDI(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        self.registers[operand_a] = operand_b
+
+    def PRN(self):
+        operand_a = self.ram_read(self.pc + 1)
+        print(self.registers[operand_a])
+
+    def HLT(self):
+        exit()
+
+    def MUL(self):
+        operand_1 = self.ram_read(self.pc + 1)
+        operand_2 = self.ram_read(self.pc + 2)
+        self.alu("MUL", operand_1, operand_2)
+    
+    def DIV(self):
+        operand_1 = self.ram_read(self.pc + 1)
+        operand_2 = self.ram_read(self.pc + 2)
+        self.alu("DIV", operand_1, operand_2)
+
+    def ADD(self):
+        operand_1 = self.ram_read(self.pc + 1)
+        operand_2 = self.ram_read(self.pc + 2)
+        self.alu("ADD", operand_1, operand_2)
+
+    def SUB(self):
+        operand_1 = self.ram_read(self.pc + 1)
+        operand_2 = self.ram_read(self.pc + 2)
+        self.alu("SUB", operand_1, operand_2)
 
     def run(self):
         """Run the CPU.
@@ -99,26 +173,35 @@ class CPU:
         The number of bytes an instruction uses can be determined from the two 
         high bits (bits 6-7) of the instruction opcode. See the LS-8 spec for details.
         """
-        pc = 0 # Program Counter, address of the currently executing instruction
-        running = True
 
-        while running:
-            ir = self.ram_read(self.pc) # Instruction Register, contains a copy of the currently executing instruction
+        self.running = True
 
-            operand_1 = self.ram_read(self.pc + 1)
-            operand_2 = self.ram_read(self.pc + 2)
+        # while self.running:
+
+        #     ir = self.ram_read(self.pc) # Instruction Register, contains a copy of the currently executing instruction
             
-            if ir == '10000010': # LDI
-                self.registers[operand_1] = operand_2
-                pc += 3
+        #     operand_1 = self.ram_read(self.pc + 1)
+        #     operand_2 = self.ram_read(self.pc + 2)
+            
+        #     if ir == 0b10000010: # LDI
+        #         self.registers[operand_1] = operand_2
+        #         self.pc += 3
 
-            elif ir == '01000111': # PRN
-                print(self.registers[operand_1])
-                pc += 2
+        #     elif ir == 0b01000111: # PRN
+        #         print(self.registers[operand_1])
+        #         self.pc += 2
 
-            elif ir == '00000001': # HLT
-                running = False
-                pc += 1
+        #     elif ir == 0b00000001: # HLT
+        #         self.running = False
+        #         self.pc += 1
 
-            else:
-                print(f"Unknown instruction")
+        #     else:
+        #         print(f"Unknown instruction")
+
+        while self.running:
+            ir = self.ram_read(self.pc)
+            self.opcodes[ir]()
+            number_of_operands = (ir & 0b11000000) >> 6
+            how_far_to_move_pc = number_of_operands + 1
+            self.pc += how_far_to_move_pc
+
